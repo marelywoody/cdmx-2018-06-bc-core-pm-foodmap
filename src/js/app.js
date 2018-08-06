ea8e0ae6375c78906849874d14c8f6760f4a8653
marelywoody <ros_ely_arc@hotmail.com>
2018-08-06 01:29:59 -0500

    google maps

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+window.onload = () => {
+  window.setTimeout('redirect()', 2000);
+};
+
+const redirect = () => {
+  window.location = '../views/view.html';
+};
---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+let search1 = document.getElementById('search-food');
+
+search1.addEventListener('click', event => {
+  let search2 = search1.value;
+});
+
+let map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 19.4978, lng: -99.1269},
+    zoom: 8
+  });
+};
